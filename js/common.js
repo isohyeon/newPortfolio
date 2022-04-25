61bb975748a2d53d56ab75018ca4d1d3fe95b364
sohyeon <sohyun9743@naver.com>
2022-04-26 00:05:52 +0900

    script 수정
    script 코드 분리

--- js/common.js
+++ js/common.js
@@ -1,15 +1,47 @@
-"use strict";
+'use strict';
 
-(function ($) {
-  $(function () {
-    $(document).ready(function ($) {
-      $("tbody .table-row").click(function () {
-        var $_datahref = $(this).data("href");
-        if (typeof $_datahref !== "undefined") {
-          window.document.location = $(this).data("href");
-        }
-      });
-    });
+var body = window.opera ? document.compatMode == "CSS1Compat" ? $('html') : $('body') : $('html,body'),
+    section = $('section');
+
+var numOfPages = section.length - 1,
+    curPage = 0,
+    scrollLock = false;
+
+function scrollPage() {
+
+  $(document).on("mousewheel DOMMouseScroll", function (e) {
+    if (scrollLock) return;
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) navigateUp();else navigateDown();
   });
-})(jQuery);
+
+  $(document).on("keydown", function (e) {
+    if (scrollLock) return;
+    if (e.which === 38) navigateUp();else if (e.which === 40) navigateDown();
+  });
+}
+
+function pagination() {
+  scrollLock = true;
+  body.stop().animate({
+    scrollTop: section.eq(curPage).offset().top
+  }, 600, 'swing', function () {
+    scrollLock = false;
+  });
+};
+
+function navigateUp() {
+  if (curPage === 0) return;
+  curPage--;
+  pagination();
+};
+
+function navigateDown() {
+  if (curPage === numOfPages) return;
+  curPage++;
+  pagination();
+};
+
+$(function () {
+  scrollPage();
+});
 //# sourceMappingURL=common.js.map
